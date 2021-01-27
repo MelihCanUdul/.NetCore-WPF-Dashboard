f876adfca450c1dc21116d960cd38aba1e700bd2
MelihCanUdul <melihh.2580@gmail.com>
2021-01-27 11:05:11 +0300

    Project added

---
+++ Dashboard1/Dashboard1/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard1
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Consumo consumo = new Consumo();
+            DataContext = new ConsumoViewModel(consumo);
+        }
+
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void GridBarraTitulo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+           
+            Window2 form = new Window2();
+           
+            form.Show(); 
+            
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+    }
+
+    internal class ConsumoViewModel
+    {
+        public List<Consumo> Consumo { get; private set; }
+
+        public ConsumoViewModel(Consumo consumo)
+        {
+            Consumo = new List<Consumo>();
+            Consumo.Add(consumo);
+        }
+    }
+
+    internal class Consumo
+    {
+        public string Titulo { get; private set; }
+        public int Porcentagem { get; private set; }
+
+        public Consumo()
+        {
+            Titulo = "Consumo Atual";
+            Porcentagem = CalcularPorcentagem();
+        }
+
+        private int CalcularPorcentagem()
+        {
+            return 47; //Calculo da porcentagem de consumo
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("adsadas");
+        }
+    }
+}
